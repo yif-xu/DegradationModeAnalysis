566939ce27108db42fe0f64ec8de8a4c4af9b864
Yifan Xu <yifanxu@kth.se>
2026-08-17 15:38:19 +0200

    为了画出不同的ocp进行比较

--- scripts/plotter.m
+++ scripts/plotter.m
@@ -1,10 +1,16 @@
 figure; hold on;
-P45B_deli=load('/Users/yifanxu/Vscode/DegradationModeAnalysis/input_data/silicon-graphite/P45B_Anode_Delithiation_0C03.mat')
-P45B_li=load('/Users/yifanxu/Vscode/DegradationModeAnalysis/input_data/silicon-graphite/P45B_Anode_Lithiation_0C03.mat')
-plot(s_lith.s.normalizedCapacity, s_lith.s.voltage, '-b', 'LineWidth', 1.2, 'DisplayName', 'lith');
-plot(s_delith.s.normalizedCapacity, s_delith.s.voltage,  '-r', 'LineWidth', 1.2, 'DisplayName', 'delith');
-plot(P45B_deli.P45B_Anode_Delithiation_0C03.normalizedCapacity, P45B_deli.P45B_Anode_Delithiation_0C03.voltage, '-g', 'LineWidth', 1.2, 'DisplayName', 'P45B-delith');
-plot(P45B_li.P45B_Anode_Lithiation_0C03.normalizedCapacity, P45B_li.P45B_Anode_Lithiation_0C03.voltage, '-m', 'LineWidth', 1.2, 'DisplayName', 'P45B-lith');
+P45B_deli=load('/Users/yifanxu/Vscode/DegradationModeAnalysis/input_data/silicon/Si_Delithiation_Li.mat');
+P45B_li=load('/Users/yifanxu/Vscode/DegradationModeAnalysis/input_data/silicon/Si_Lithiation_Li.mat');
+s_lith_95 = load('/Users/yifanxu/Vscode/DegradationModeAnalysis/MySilicon/bak4695_lith_ocp.mat');
+s_delith_95 = load('/Users/yifanxu/Vscode/DegradationModeAnalysis/MySilicon/bak4695_delith_ocp.mat');
+s_lith_90 = load('/Users/yifanxu/Vscode/DegradationModeAnalysis/MySilicon/bak4690_lith_ocp.mat');
+s_delith_90 = load('/Users/yifanxu/Vscode/DegradationModeAnalysis/MySilicon/bak4690_delith_ocp.mat');
+plot(s_lith_95.siliconStruct.normalizedCapacity, s_lith_95.siliconStruct.voltage, '-c', 'LineWidth', 1.2, 'DisplayName', '4695-lith-5.5 wt%');
+plot(s_delith_95.siliconStruct.normalizedCapacity, s_delith_95.siliconStruct.voltage,  '-y', 'LineWidth', 1.2, 'DisplayName', '4695-delith-5.5 wt%');
+plot(s_lith_90.siliconStruct.normalizedCapacity, s_lith_90.siliconStruct.voltage, '-b', 'LineWidth', 1.2, 'DisplayName', '4690-lith-8.28 wt%');
+plot(s_delith_90.siliconStruct.normalizedCapacity, s_delith_90.siliconStruct.voltage,  '-r', 'LineWidth', 1.2, 'DisplayName', '4690-delith-8.28 wt%');
+plot(P45B_deli.Si_Delithiation_Li.normalizedCapacity, P45B_deli.Si_Delithiation_Li.voltage, '-g', 'LineWidth', 1.2, 'DisplayName', 'P45B-delith');
+plot(P45B_li.Si_Lithiation_Li.normalizedCapacity, P45B_li.Si_Lithiation_Li.voltage, '-m', 'LineWidth', 1.2, 'DisplayName', 'P45B-lith');
 xlabel('Normalized Capacity');
 ylabel('Voltage (V)');
 legend('Location','best');
